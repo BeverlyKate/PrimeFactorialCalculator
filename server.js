b2e66c3b0f0f95cc989b6a9c8ad7d278205f1ac4
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2025-06-21 00:17:51 +0800

    vercel config changes

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const app = express();
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 app.use("/src", express.static("src"));
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.render("prime-factorial");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  `Server started on port ${port}`;
+});
