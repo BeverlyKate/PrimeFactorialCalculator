9d6fc536bfae9de696418b1a06ac19671e550c63
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2025-06-21 15:53:50 +0800

    Render commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
@@ -10,6 +11,12 @@ app.get("/", (req, res) => {
   res.render("prime-factorial");
 });
 
+<<<<<<< Updated upstream
 app.listen(port, () => {
   `Server started on port ${port}`;
+=======
+//app.listen(3000);
+app.listen(port, () => {
+  console.log("Server started on port 8080");
+>>>>>>> Stashed changes
 });
--- src/logic.js
+++ src/logic.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     const input = getInput();
 
     if (input == 1) {
-      result = "1 is not prime, nor is it a composite number.";
+      result = "1 is not prime, nor is it a composite number."; //this could have been handled better setting isPrime to have 3 values (1,2,3) rather than having it be a boolean.
     } else {
       result = isItPrime(input);
     }
@@ -21,16 +21,20 @@ $(document).ready(function () {
     console.log(getFactorialNaive(input));
   });
 
-  //helper functions
+  ////helper functions/////
+
+  //Reusable function that gets the user input
   function getInput() {
     return $("#numberInput").val();
   }
 
+  //Adds the results to the front end.
   function addToHistory(result) {
     var resultHolder = `<div id="resultHistory"> ${result}</div>`;
     $("#resultHolder").append(resultHolder);
   }
 
+  //this function is the logic to see if the given input is prime or not.
   function isItPrime(input) {
     var isPrime;
     if (input <= 1) {
@@ -49,12 +53,15 @@ $(document).ready(function () {
       return input + " is composite!";
     }
   }
-
+  //The AKS test is a 100% accurate way to determining if a number is prime or not, published in a paper in 2004.
+  //The idea is to take the number p, and create p rows of Pascal's triangle.
+  //If all the numbers/coefficients (in the context of polynomials) are divisible by the given number p, then the number is prime.
+  //source: https://youtu.be/HvMSRWTE2mI?si=w2ueYq6DA2Q6ejVl
   function AKStest(input) {
     var coefArr = [1];
     var isDivisible = true;
-
-    //task: expand (x-1)^-(x^p-1) where p= input.
+    //to solve this, i broke the problem down into two parts: generating the pascal triangle and then evaluating if the lowest row is
+    //task: expand (x-1)^p where p= input.
     for (let i = 0; i < input; i++) {
       coefArr = evaluateRow(coefArr);
     }
@@ -62,6 +69,7 @@ $(document).ready(function () {
     //check if coefArr is divisible by the input
     var i = 1;
     while (isDivisible && i <= coefArr.length - 2) {
+      //while loop used to escape the function early as soon as at least one non-divisible coefficient is encountered.
       if (coefArr[i] % input != 0) {
         isDivisible = false;
         console.log(coefArr[i]);
@@ -73,13 +81,15 @@ $(document).ready(function () {
     return isDivisible;
   }
 
+  //evaluate row is a helper function used in the AKS test. it forms the pascal triangle used in expanding polynomials.
   function evaluateRow(array) {
     var nextRow = [];
     for (let i = 0; i < array.length; i++) {
+      //for all indeces of the previous row, except for the outermost edges (1 and 1)
       if (i == 0) {
         nextRow.push(1);
       } else {
-        nextRow.push(array[i - 1] + array[i]);
+        nextRow.push(array[i - 1] + array[i]); //add the two numbers above the current index, you cen see my logic in the google drive
       }
     }
     nextRow[nextRow.length] = 1;
