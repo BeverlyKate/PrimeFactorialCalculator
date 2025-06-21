5b88bc7f545d145f152aeff08ffa5ed7e878b78b
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2025-06-21 16:25:37 +0800

    Render commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
@@ -11,12 +10,7 @@ app.get("/", (req, res) => {
   res.render("prime-factorial");
 });
 
-<<<<<<< Updated upstream
-app.listen(port, () => {
-  `Server started on port ${port}`;
-=======
 //app.listen(3000);
 app.listen(port, () => {
   console.log("Server started on port 8080");
->>>>>>> Stashed changes
 });
